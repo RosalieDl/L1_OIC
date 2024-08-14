d49d429bf4706db71c6f9fdecce6dd01dc959a11
Rosalie Duteuil <rosa-lee@live.fr>
2024-08-14 14:24:48 +0200

    ajout de la vidéo sur les entiers

--- pages/pi.py
+++ pages/pi.py
@@ -1,13 +1,3 @@
-import streamlit as st
-from mpmath import mp
-
-def somme_pi(length: int):
-    mp.dps = length + 1
-    pi = mp.pi
-    somme = 0
-    for digit in str(pi)[2:]:
-        somme += int(digit)
-    return(somme)
-
-print(somme_pi(20))
-print(somme_pi(12*12))
+version https://git-lfs.github.com/spec/v1
+oid sha256:afe2f1c1b51c7f5a25b333834b523e7ff9580d387dd74d26468ef0bbdd712cd0
+size 246
